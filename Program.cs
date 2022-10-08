244aac564592a29d34da23939db0ca8b44db25bc
Jeff <83289672+jeffmahay@users.noreply.github.com>
2022-10-07 22:48:41 -0600

    WEDIDITBOYS

--- Program.cs
+++ Program.cs
@@ -8,55 +8,52 @@
         // Initial Score //
         int score = 300;
 
+        // Initial choice
+        bool stop = false;
+
         // Creates a variable to hold the deck of cards created in the CardDeck file //
         List<string> cards = deck.cards;
 
-        // Getting the first card
-        string firstCard = cardDrawn(cards);
-
-        // Getting the drawn cards
-        List<string> drawn = new List<string>();
-        drawn.Add(firstCard);
-
-        // Initial choice
-        bool choice = false;
-
-
-
+        // Initial card drawn from the deck //
+        string first = deck.draw(cards);
+        
         // Keep playing the game until its over //
-        while(IsOver(score, cards, choice) != true)
-        {
-            int change = terminalDialogue(score, cards, drawn);
+        while(stop != true)
+        { 
+            string second = deck.draw(cards);
+
+            int change = terminalDialogue(score, cards, first, second);
+            
+            score = score + change;
+            Console.WriteLine($"Your score is {score}");
+
+            first = second;
+
             bool decision = playAgain();
-            int points = score + change;
-            IsOver(points, cards, decision);
+            
+            stop = IsOver(score, cards, decision);
         }
+
+        Console.WriteLine("Thanks for playing!");
         
     }
 
     // Function that checks if the conditions of a game over are met //
-    public static bool IsOver(int score, List<string> cards, bool choice)
+    public static bool IsOver(int score, List<string> cards, bool decision)
     {
         CardDeck deck = new CardDeck();
 
-        if(score > 0)
-        {
-            return false;
-        }
-        else if(deck.draw(cards) == "over")
+        if(deck.draw(cards) == "over")
         {
             return true;
         }
-<<<<<<< HEAD
-=======
-        else if(choice == true)
+        else if(decision == false || score == 0)
         {
-            return false;
+            return true;
         }
->>>>>>> 2502355d005f3f6348760317196c5066905dcb0f
         else
         {
-            return true;
+            return false;
         }   
     }
 
@@ -71,39 +68,41 @@
     }
 
     // Holds all the console.writeline code, as well as the score calculator. //
-    public static int terminalDialogue(int score, List<string> cards, List<string>drawn)
-    {
+    public static int terminalDialogue(int score, List<string> cards, string first, string second)
+    {   
         CardDeck deck = new CardDeck();
 
-        string next = cardDrawn(cards);
-        
-        int first = deck.convert(drawn[-1]);
-        int second = deck.convert(next);
+        Console.WriteLine($"The last card is: {first}");
+        Console.WriteLine($"Higher or lower? [h/l]:");
 
-        Console.WriteLine($"The last card is: {drawn[-1]}");
-        Console.WriteLine($"Higher or lower? [h/l]");
         string? input = Console.ReadLine() ?? "";
-        Console.WriteLine($"The next card is: {next}");
-        int change = scoreCalculator(input, first, second, score);
+
+        Console.WriteLine($"The next card is: {second}");
+
+        int convfirst = deck.convert(first);
+        int convsecond = deck.convert(second);
+
+        int change = scoreCalculator(input, convfirst, convsecond);
+
         return change;
     }
 
     // checks if the first instance of the deck drawn is greater or lower, and compares that with the user's input //
-    public static int scoreCalculator(string input, int first, int second, int score)
+    public static int scoreCalculator(string input, int convfirst, int convsecond)
     {
-        if(input == "h" && second > first)
+        if(input == "h" && convsecond > convfirst)
         {
             return 100;
         }
-        else if(input == "h" && second < first)
+        else if(input == "h" && convsecond < convfirst)
         {
             return -75;
         }
-        else if(input == "l" && second < first)
+        else if(input == "l" && convsecond < convfirst)
         {
             return 100;
         }
-        else if(input == "l" && second > first)
+        else if(input == "l" && convsecond > convfirst)
         {
             return -75;
         }
@@ -124,9 +123,9 @@
     {
         Console.WriteLine("Play again? [y/n]");
 
-        string? input = Console.ReadLine();
+        string? continueChoice = Console.ReadLine();
 
-        if(input == "y")
+        if(continueChoice == "y")
         {
             return true;
         }
